cc8b4dcbcfc7c88d4b729697f69d9d53587c38f6
slz <88058173@qq.com>
2022-03-04 17:50:20 +0800

    3.4-baidu用例案例

--- firstdemo/unittest/demo.py
+++ firstdemo/unittest/demo.py
@@ -1,24 +1,59 @@
 import unittest
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
 
 class demo(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls) :
-        print("start---")
+    # @classmethod
+    # def setUpClass(cls) :
+    #     cls.driver = webdriver.Chrome()
+    #     cls.driver.get("https://www.baidu.com/")
+    #
+    # @classmethod
+    # def tearDownClass(cls):
+    #     print("end---")
 
-    @classmethod
-    def tearDownClass(cls):
-        print("end---")
+    def setUp(self) -> None:
+        print("---start---")
+        self.driver = webdriver.Chrome()
+        self.url = "https://www.baidu.com/"
+        time.sleep(1)
 
-    def testFunc1(self):
-        print("testFunction1")
-        self.assertNotEqual(1,2)
+    def tearDown(self) -> None:
+        time.sleep(1)
+        self.driver.quit()
+        print("---end---")
 
-    def testFunc2(self):
-        print("testFunction2")
-        self.assertEqual(1,2)
+    def test_login_success(self):
+        '''
+        正确用户名/密码，登陆成功
+        :return:
+        '''
+        self.driver.get(self.url)
+        self.driver.find_element("id","s-top-loginbtn").click()
+        time.sleep(1)
+        self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__userName"]').send_keys("admin")
+        self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__password"]').send_keys("admin")
+        login_submit = self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__password"]')
+        print(login_submit)
+        login_name = self.driver.find_element("id",'login_name').text
+        self.assertEqual("admin",login_name)
+
+    def test_login_failed_without_username(self):
+        '''
+        用户名为空，登录失败
+        :return:
+        '''
+        self.driver.get(self.url)
+        self.driver.find_element("id","s-top-loginbtn").click()
+        time.sleep(1)
+        self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__password"]').send_keys("admin")
+        login_submit = self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__password"]')
+        login_submit.click()
+        errmsg = self.driver.find_element("xpath",'//*[@id="TANGRAM__PSP_11__error"]').text
+        self.assertEqual(errmsg,"请您输入手机号/用户名/邮箱")
 
 if __name__ == '__main__':
-    print("diaoyong")
+    print("---main调用---")
     unittest.main()
