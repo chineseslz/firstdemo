c32fa6099e37afd4b924e804eeeedf1d468cf038
slz <88058173@qq.com>
2022-03-10 17:51:38 +0800

    3.10-ddtDemo

--- firstdemo/unittestDemo/smoke/numberTests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class numberTests(unittest.TestCase):
-
-    def testNum1(self):
-        self.assertIn("hello","hello world")
-
-    def testNum2(self):
-        self.assertNotIn("hello","hello world")
--- firstdemo/unittestDemo/smoke/stringTests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-class stringTests(unittest.TestCase):
-
-    def testStr1(self):
-        self.assertIn("hello","hello world")
-
-    def testStr2(self):
-        self.assertNotIn("hello","hello world")
