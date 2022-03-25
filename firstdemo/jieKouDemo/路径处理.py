ec8da706f8b152cec98bcb85ee5dad6a271c54e6
slz <88058173@qq.com>
2022-03-25 13:45:59 +0800

    3.25-路径处理

---
+++ firstdemo/jieKouDemo/路径处理.py
@@ -0,0 +1,12 @@
+import os
+
+p1 = os.path.abspath("code.txt")   #绝对路径
+p1 = os.path.abspath(".")   # 根路径
+p1 = os.path.dirname(p1)  #获取所在目录的路径 （上级目录绝对路径）
+print(p1)
+
+#获取项目的跟目录路径
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+#获取永猎数据文件夹所在的文件目录
+data_dir = os.path.join(base_path,"pytestDemo")
