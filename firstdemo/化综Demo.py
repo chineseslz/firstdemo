64154c4241b76ca481a10e28e9de994571d886fd
slz <88058173@qq.com>
2022-04-01 11:53:50 +0800

    4/1-时间demo测试

--- firstdemo/化综Demo.py
+++ firstdemo/化综Demo.py
@@ -15,7 +15,7 @@ driver.get(url) #连接地址
 # print(driver.title)    #输出浏览器标题
 
 #登录
-time.sleep(3)
+time.sleep(2)
 login_user = driver.find_elements(By.CLASS_NAME,"el-input__inner")[0]
 login_user.send_keys("admin")
 login_pwd = driver.find_elements(By.CLASS_NAME,"el-input__inner")[1]
@@ -25,10 +25,19 @@ login_btn.click()
 time.sleep(2)
 driver.find_element(By.XPATH,"//*[text()='企业生产全流程']").click()
 time.sleep(1)
-driver.find_element(By.XPATH,"//*[text()='设备设施管理']").click()
+driver.find_element(By.XPATH,"//*[text()='作业安全']").click()
 time.sleep(1)
-driver.find_element(By.XPATH,"//*[text()='设备设施类型']").click()
+driver.find_element(By.XPATH,"//*[text()='特殊作业申请']").click()
+time.sleep(1)
+datetime1 = driver.find_element(By.XPATH,'//*[@id="pane-pos/private/private-list"]/div/div/div/form/div[3]/div/div/input[1]')
+datetime1.click()
+datetime1.send_keys("2022-03-31 00:00:00")
+datetime2 = driver.find_element(By.XPATH,'//*[@id="pane-pos/private/private-list"]/div/div/div/form/div[3]/div/div/input[2]')
+datetime2.click()
+datetime2.send_keys("2022-04-21 00:00:00")
+button = driver.find_element(By.XPATH,"//*[text()='查询']").click()
 
 
-time.sleep(5)
-driver.quit()
+#
+# time.sleep(5)
+# driver.quit()
