d5fc04081539a2d38ab329facf5d083cf62e216c
slz <88058173@qq.com>
2022-04-02 17:57:21 +0800

    4.2-注册接口的实现

--- firstdemo/jieKouDemo/shiZhanDemo/testcases/test_recharge.py
+++ firstdemo/jieKouDemo/shiZhanDemo/testcases/test_recharge.py
@@ -4,18 +4,18 @@
 '''
 import unittest
 import os
-from unittestreport import *
+from unittestreport import ddt,list_data
 from ..common.handle_excel import HandleExcel
 from ..common.handle_path import DATA_DIR
 
 @ddt
 class TestRecharge(unittest.TestCase):
     execl = HandleExcel(os.path.join(DATA_DIR,"apicases.xlsx"),'recharge')
-    cases = execl.read_data
+    cases = execl.read_data()
 
     @classmethod
     def setUpClass(cls):
-        pass
+        """"""
 
     @list_data(cases)
     def test_recharge(self,item):
